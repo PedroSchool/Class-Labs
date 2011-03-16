c8d5e2c50ab0cd6668aaccb1231b2c62b160ead3
Pedro Enrique <pec1985@yahoo.com>
2011-03-16 12:18:59 -0400

    "Print Your Name" added

---
+++ Print Your Name/main.cpp
@@ -0,0 +1,17 @@
+// Description: Prints name on console
+// Name: Print Your Name
+// Developer: Pedro Enrique
+// Last Modified 3/16/2011
+
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Pedro\n";
+    
+    // if Windows: system("pause");
+    return 0;
+}
+
