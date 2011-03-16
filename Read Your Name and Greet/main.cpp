b7710bf45f17c8d9f140c1607e21702c5606a60c
Pedro Enrique <pec1985@yahoo.com>
2011-03-16 12:43:48 -0400

    "Read your name and greet" added

---
+++ Read Your Name and Greet/main.cpp
@@ -0,0 +1,24 @@
+// Description: Read your name and greet
+// Name: Read you name and greet
+// Developer: Pedro Enrique
+// Last Modified 3/15/2011
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    const int SIZE = 20;
+    char a[SIZE];
+    
+    cout << "\t\tThe greeting lab \n"
+         << "\t\t  by Pedro \n" << endl;
+    
+    cout << "What is your name?"<<endl;
+    cin >> a;
+    cout << "Hello " << a << "\n" << endl;
+    
+    // if Windows: system("pause");
+    
+    return 0;
+}
+
